5f1761c0434c62e752bec461c3470c5109a5b237
JHun273 <rlawogns273@gmail.com>
2024-03-19 12:39:48 +0900

    spring start

---
+++ Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/SunflowerSpringApplication.java
@@ -0,0 +1,13 @@
+package Sunflower.Sunflowerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunflowerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunflowerSpringApplication.class, args);
+	}
+
+}

6ff7a0add33f7e4adc0df27795689944508d8274
JHun273 <rlawogns273@gmail.com>
2024-04-13 13:55:46 +0900

    영화3개 넘기면 바로 가져다 주는거

---
+++ Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/controller/Favorite_Movie_Controller.java
@@ -0,0 +1,26 @@
+package Sunflower.Sunflowerspring.controller;
+
+import Sunflower.Sunflowerspring.dto.Favorite_Genres_Dto;
+import Sunflower.Sunflowerspring.dto.Favorite_Movie_Dto;
+import Sunflower.Sunflowerspring.service.Favorite_Genres_Service;
+import Sunflower.Sunflowerspring.service.Favorite_Movie_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = {"https://resttesttest.com/", "http://127.0.0.1/", "https://125.178.91.189/", "http://125.178.91.189", "125.178.91.189"}) // 컨트롤러에서 설정
+@RestController
+public class Favorite_Movie_Controller {
+
+    private final Favorite_Movie_Service favorite_movie_service;
+
+    @Autowired
+    public Favorite_Movie_Controller(Favorite_Movie_Service favorite_movie_service) { this.favorite_movie_service = favorite_movie_service;    }
+
+    @PostMapping("favoritemovie")
+    public String returnMovie( Favorite_Movie_Dto favorite_movie_dto)  {
+
+        return favorite_movie_service.returnMovie(favorite_movie_dto);
+
+    }
+
+}
---
+++ Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/dto/Favorite_Movie_Dto.java
@@ -0,0 +1,41 @@
+package Sunflower.Sunflowerspring.dto;
+
+public class Favorite_Movie_Dto {
+
+    private Long mid;
+    private String movie1;
+    private String movie2;
+    private String movie3;
+
+    public Long getMid() {
+        return mid;
+    }
+
+    public void setMid(Long mid) {
+        this.mid = mid;
+    }
+
+    public String getMovie1() {
+        return movie1;
+    }
+
+    public void setMovie1(String movie1) {
+        this.movie1 = movie1;
+    }
+
+    public String getMovie2() {
+        return movie2;
+    }
+
+    public void setMovie2(String movie2) {
+        this.movie2 = movie2;
+    }
+
+    public String getMovie3() {
+        return movie3;
+    }
+
+    public void setMovie3(String movie3) {
+        this.movie3 = movie3;
+    }
+}

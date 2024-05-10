90f3df136869eb2e96f62efd5e983dc1eadc6da7
kae-mi <rudah7815@naver.com>
2024-05-11 02:23:40 +0900

    sd

--- Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/controller/UserController.java
+++ Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/controller/UserController.java
@@ -5,12 +5,14 @@ import Sunflower.Sunflowerspring.domain.UserJoinRequest;
 import Sunflower.Sunflowerspring.domain.UserLoginRequest;
 import Sunflower.Sunflowerspring.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {"https://resttesttest.com/", "http://127.0.0.1/", "localhost", "localhost:80", "http://localhost", "https://localhost", "localhost:443", "https://movie-recommendation.kro.kr"}) // 컨트롤러에서 설정
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -23,6 +25,7 @@ public class UserController {
 
     @PostMapping("/api/login") //로그인 요청하면 token을 발급함 아직 구현 안함
     public ResponseEntity<String> login(UserLoginRequest userLoginRequest) {
+        log.info(userLoginRequest.getUserName(), userLoginRequest.getPassword());
         String token = userService.login(userLoginRequest); //로그인에 성공하면 token리턴함
         return ResponseEntity.ok().body(token); //성공하면 body에 token 실어서 넘김
     }
--- Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/domain/UserLoginRequest.java
+++ Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/domain/UserLoginRequest.java
@@ -2,9 +2,11 @@ package Sunflower.Sunflowerspring.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class UserLoginRequest {
     private String userName;
     private String password;

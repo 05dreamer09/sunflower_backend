f059d613b5c7f9e91ca5dc3d15fb6c610100bb87
kae-mi <rudah7815@naver.com>
2024-05-11 05:36:19 +0900

    sd

--- Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/controller/ReviewController.java
+++ Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/controller/ReviewController.java
@@ -1,11 +1,16 @@
 package Sunflower.Sunflowerspring.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"https://resttesttest.com/", "http://127.0.0.1/", "localhost", "localhost:80", "http://localhost", "https://localhost", "localhost:443", "https://movie-recommendation.kro.kr"}) // 컨트롤러에서 설정
 @RestController
+@RequiredArgsConstructor
+
 public class ReviewController {
 
     @PostMapping("/api/reviews")

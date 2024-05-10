641134e46dba67c894be554d0e220610c049c1fc
kae-mi <rudah7815@naver.com>
2024-05-10 18:31:13 +0900

    sd

--- Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/domain/UserJoinRequest.java
+++ Sunflower-spring/src/main/java/Sunflower/Sunflowerspring/domain/UserJoinRequest.java
@@ -3,10 +3,12 @@ package Sunflower.Sunflowerspring.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class UserJoinRequest {
     private String userName;
     private String password;
